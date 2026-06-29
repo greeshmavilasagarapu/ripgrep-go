2d750f81d7c3aa60bee81de51ebd56b58d5e1f95
Grishima <grishima@b68.dev>
2026-06-29 16:13:11 +0000

    Implementation 1

--- main.go
+++ main.go
@@ -3,77 +3,32 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
-// type Found struct {
-// 	Line  int16
-// 	Cntnt string
-// }
-
-// type File struct {
-// 	Name string
-// 	Res  []Found
-// }
-
-// var (
-// 	files = []File{}
-// )
-// var(
-// 	File = make(map[string]string)
-// )
-// func searchFile(current_dir string, word string) {
-// 	entries, _ := os.ReadDir(current_dir)
-// 	for _, entry := range entries {
-// 		if entry.IsDir() {
-// 			searchFile(current_dir+"/"+entry.Name(), word)
-// 		} else {
-// 			if strings.Contains(entry.Name(), ".") {
-// 				data, _ := os.ReadFile(current_dir + "/" + entry.Name())
-// 				if strings.Contains(string(data), word) {
-// 					lines := strings.Split(string(data), "\n")
-// 					foundlines := []Found{}
-// 					for i, line := range lines {
-// 						if strings.Contains(line, word) {
-// 							foundlines = append(foundlines, Found{
-// 								Line:  int16(i) + 1,
-// 								Cntnt: line,
-// 							})
-// 						}
-// 					}
-// 					files = append(files, File{
-// 						Name: entry.Name(),
-// 						Res:  foundlines,
-// 					})
-// 				}
-// 			}
-// 		}
-// 	}
-// }
-
-//	func printing(files []File, color string, word string) {
-//		for _, file := range files {
-//			fmt.Println(file.Name)
-//			for _, val := range file.Res {
-//				Content := strings.ReplaceAll(val.Cntnt, word, color)
-//				fmt.Println(strconv.Itoa(int(val.Line)) + ":" + Content)
-//			}
-//			fmt.Print("\n")
-//		}
-//	}
 func main() {
-	//searchArr := os.Args[1:]
-	current_dir := os.Getenv("PWD")
-	fmt.Println(current_dir)
-	fmt.Println("helllo")
-	files, _ := os.ReadDir(current_dir)
-	fmt.Println(files)
-	for _, file := range files{
-		data,_ := os.ReadFile(file.Name())
-		fmt.Println(string(data))
+	// Current directory
+	word := os.Args[1]
+	currDir, _ := os.Getwd()
+	fmt.Println(currDir)
+	// Home directory from environment variable
+	home := os.Getenv("HOME")
+	fmt.Println(home)
+	// Read all files and folders in current directory
+	files, err := os.ReadDir(".")
+	if err != nil {
+		fmt.Println("Error reading directory")
+		return
 	}
-	//current_dir := "/home/coder/Projects/redis-go-impl/"
-	// searchFile(current_dir, searchArr[0])
-	// const reset = "\033[0m"
-	// const yellow = "\033[1;33m"
-	// Word := yellow + searchArr[0] + reset
+	fmt.Println("\nFiles in current directory:")
+	for _, file := range files {
+		fmt.Println(file.Name())
+		// Print only Go files
+		if strings.HasSuffix(file.Name(), ".go") {
+			data, _ := os.ReadFile(file.Name())
+			if strings.Contains(string(data), word) {
+				fmt.Println("Found in:", file.Name())
+			}
+		}
+	}
 }
